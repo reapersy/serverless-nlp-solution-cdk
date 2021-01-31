4b0954736f1a9a25cec59bb886b1967767df828d
reapersy <reapersoy@proton.me>
2021-01-31 18:01:07 +0000

    rewrite tests

--- codes/lambda/review-analysis/src/handler.py
+++ codes/lambda/review-analysis/src/handler.py
@@ -2,3 +2,24 @@ import os
 import json
 from decimal import Decimal
 import boto3
+from botocore.exceptions import ClientError
+
+
+_stream_name = os.environ.get('STREAM_NAME', 'ReviewServiceDev-ReviewAnalysisStack-Stream')
+_stream_batch_size = int(os.environ.get('STREAM_BATCH_SIZE', '10'))
+
+_comp = None
+_kinesis = None
+
+
+def get_comprehend():
+    global _comp
+    if _comp is None:
+        _comp = boto3.client('comprehend')
+    return _comp
+
+
+def get_kinesis():
+    global _kinesis
+    if _kinesis is None:
+        _kinesis = boto3.clien
