e3b4d8980105735511368d656f672521b700d771
reapersy <reapersoy@proton.me>
2021-05-17 07:50:54 +0000

    improved doc

--- infra/stack/review-dashboard-stack.ts
+++ infra/stack/review-dashboard-stack.ts
@@ -117,3 +117,197 @@ export class ReviewDashboardStack extends base.BaseStack {
 
         this.createLambdaWidget(`${baseName}Function`, functionArn);
         this.addDdbWidgets(`${baseName}Table`, tableName);
+    }
+    
+    private createAnalysisWidget(baseName: string, functionArn: string, streamName: string, hoseName: string) {
+        this.dashboard.addTextTitleWidges(`## ${baseName} Dashboard`)
+
+        this.createLambdaWidget(`${baseName}Function`, functionArn);
+        this.createKinesisStreamWidget(`${baseName}Stream`, streamName);
+        this.createKinesisHoseWidget(`${baseName}Hose`, hoseName);
+    }
+
+    private createWidgetAlarmAction(baseName: string, metric: cloudwatch.Metric, props: ApiGatewayAlarmProps, period: number, width: number, height?: number) {
+        const alarmTopic = new sns.Topic(this, `${baseName}-Alarm-Topic`, {
+            displayName: `${this.projectPrefix}-${baseName}-Alarm-Topic`,
+            topicName: `${this.projectPrefix}-${baseName}-Alarm-Topic`
+        });
+
+        const emailList: string[] = props.subscriptionEmails;
+        emailList.forEach(email => alarmTopic.addSubscription(new subscriptions.EmailSubscription(email)));
+
+        const metricAlarm = metric.createAlarm(this, `${baseName}-Alarm-Metric`, {
+            alarmName: `${this.projectPrefix}-${baseName}-Alarm`,
+            threshold: props.alarmThreshold,
+            evaluationPeriods: period,
+            actionsEnabled: true,
+            alarmDescription: `This alarm occurs when ${baseName} is over ${props.alarmThreshold} for ${period} minutes.`
+        });
+        metricAlarm.addAlarmAction(new cw_actions.SnsAction(alarmTopic));
+
+        this.dashboard.addWidgets(new cloudwatch.AlarmWidget({
+            title: baseName,
+            alarm: metricAlarm,
+            width: width,
+            height: height
+        }));
+    }
+
+    private createLambdaWidget(widgetName: string, lambdaArn: string) {
+        let functionName = lambda.Function.fromFunctionArn(
+            this,
+            widgetName,
+            lambdaArn
+        ).functionName;
+
+        this.dashboard.addWidgets(
+            this.dashboard.createWidget(`${widgetName}-Invocations`, [
+                this.dashboard.createLambdaMetric(functionName, 'Invocations', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT }),
+                this.dashboard.createLambdaMetric(functionName, 'ProvisionedConcurrencyInvocations', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT })
+            ], 12),
+            this.dashboard.createWidget(`${widgetName}-ConcurrentExecutions`, [
+                this.dashboard.createLambdaMetric(functionName, 'ConcurrentExecutions', { statistic: 'Maximum', unit: cloudwatch.Unit.COUNT }),
+                this.dashboard.createLambdaMetric(functionName, 'ProvisionedConcurrentExecutions', { statistic: 'Maximum', unit: cloudwatch.Unit.COUNT }),
+                this.dashboard.createLambdaMetric(functionName, 'ProvisionedConcurrencyUtilization', { statistic: 'Maximum', unit: cloudwatch.Unit.COUNT }),
+            ], 12),
+            this.dashboard.createWidget(`${widgetName}-Duration`, [
+                this.dashboard.createLambdaMetric(functionName, 'Duration', { statistic: 'Average', unit: cloudwatch.Unit.MILLISECONDS }),
+                this.dashboard.createLambdaMetric(functionName, 'Duration', { statistic: 'Minimum', unit: cloudwatch.Unit.MILLISECONDS }),
+                this.dashboard.createLambdaMetric(functionName, 'Duration', { statistic: 'Maximum', unit: cloudwatch.Unit.MILLISECONDS })
+            ], 8),
+            this.dashboard.createWidget(`${widgetName}-Errors`, [
+                this.dashboard.createLambdaMetric(functionName, 'Errors', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT, color: '#ff0000' }),
+            ], 8),
+            this.dashboard.createWidget(`${widgetName}-Throttles`, [
+                this.dashboard.createLambdaMetric(functionName, 'Throttles', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT,  color: '#ff0000' }),
+            ], 8),
+        );
+    }
+
+    private createCognitoWidget(baseName: string, userPool: string, userPoolClient: string) {
+        this.dashboard.addTextTitleWidges(`## ${baseName} Dashboard`)
+
+        const signUpMetric = this.dashboard.createCognitoUserPoolMetric(userPool, userPoolClient, 'SignUpSuccesses', { statistic: 'Average', unit: cloudwatch.Unit.COUNT })
+        const signInMetric = this.dashboard.createCognitoUserPoolMetric(userPool, userPoolClient, 'SignInSuccesses', { statistic: 'Average', unit: cloudwatch.Unit.COUNT })
+        const tokenRefreshMetric = this.dashboard.createCognitoUserPoolMetric(userPool, userPoolClient, 'TokenRefreshSuccesses', { statistic: 'Average', unit: cloudwatch.Unit.COUNT })
+
+        this.dashboard.addWidgets(
+            this.dashboard.createWidget(`SignUpSuccesses`, [signUpMetric], 8),
+            this.dashboard.createWidget(`SignInSuccesses`, [signInMetric], 8),
+            this.dashboard.createWidget(`TokenRefreshSuccesses`, [tokenRefreshMetric], 8)
+        );
+    }
+
+    private addDdbWidgets(baseName: string, tableName: string) {
+        const consumedReadSumMetric = this.dashboard.createDynamoDBMetric(tableName, 'ConsumedReadCapacityUnits', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT });
+        const consumedReadAverageMetric = new cloudwatch.MathExpression({
+            expression: 'x / 60',
+            usingMetrics: { 'x': consumedReadSumMetric },
+            label: 'ConsumedReadCapacityUnits',
+            period: cdk.Duration.minutes(1)
+        });
+
+        const consumedWriteSumMetric = this.dashboard.createDynamoDBMetric(tableName, 'ConsumedWriteCapacityUnits', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT });
+        const consumedWriteAverageMetric = new cloudwatch.MathExpression({
+            expression: 'x / 60',
+            usingMetrics: { 'x': consumedWriteSumMetric },
+            label: 'ConsumedWriteCapacityUnits',
+            period: cdk.Duration.minutes(1)
+        });
+
+        this.dashboard.addWidgets(
+            this.dashboard.createWidget(`${baseName}-Read-Capacity`, [
+                consumedReadAverageMetric,
+                this.dashboard.createDynamoDBMetric(tableName, 'ProvisionedReadCapacityUnits', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT }),
+            ], 6),
+            this.dashboard.createWidget(`${baseName}-Read-Throttles`, [
+                this.dashboard.createDynamoDBMetric(tableName, 'ReadThrottleEvents', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT }),
+                this.dashboard.createDynamoDBMetric(tableName, 'ThrottledRequests', { statistic: 'Average' }, 'Query'),
+            ], 6),
+            this.dashboard.createWidget(`${baseName}-Write-Capacity`, [
+                consumedWriteAverageMetric,
+                this.dashboard.createDynamoDBMetric(tableName, 'ProvisionedWriteCapacityUnits', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT })
+            ], 6),
+            this.dashboard.createWidget(`${baseName}-Write-Throttles`, [
+                this.dashboard.createDynamoDBMetric(tableName, 'WriteThrottleEvents', { statistic: 'Sum', unit: cloudwatch.Unit.COUNT }),
+                this.dashboard.createDynamoDBMetric(tableName, 'ThrottledRequests', { statistic: 'Average' }, 'PutItem'),
+            ], 6),
+        );
+    }
+
+    private createKinesisStreamWidget(baseName: string, streamName: string) {
+        const getRecordsBytes = this.dashboard.createKinesisStreamMetric(streamName, 'GetRecords.Bytes', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.BYTES
+        });
+        const putRecordsBytes = this.dashboard.createKinesisStreamMetric(streamName, 'PutRecord.Bytes', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.BYTES
+        });
+        const getRecordsCount = this.dashboard.createKinesisStreamMetric(streamName, 'GetRecords.Records', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        const putRecordsCount = this.dashboard.createKinesisStreamMetric(streamName, 'IncomingRecords', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        
+        const readExceeded = this.dashboard.createKinesisStreamMetric(streamName, 'ReadProvisionedThroughputExceeded', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        const writeExceeded = this.dashboard.createKinesisStreamMetric(streamName, 'WriteProvisionedThroughputExceeded', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        
+        this.dashboard.addWidgets(
+            this.dashboard.createLeftRightWidget(`${baseName}-Record`, [getRecordsBytes, putRecordsBytes], [getRecordsCount, putRecordsCount], 12),
+            this.dashboard.createLeftRightWidget(`${baseName}-Exceeded`, [readExceeded], [writeExceeded], 12)
+        );
+    }
+
+    private createKinesisHoseWidget(baseName: string, hoseName: string) {
+        
+        const dataReadFromKinesisStreamRecords = this.dashboard.createKinesisHoseMetric(hoseName, 'DataReadFromKinesisStream.Records', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        const deliveryToS3Records = this.dashboard.createKinesisHoseMetric(hoseName, 'DeliveryToS3.Records', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+        const succeedConversionRecords = this.dashboard.createKinesisHoseMetric(hoseName, 'SucceedConversion.Records', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.COUNT
+        });
+
+        const throttledGetRecords = this.dashboard.createKinesisHoseMetric(hoseName, 'ThrottledGetRecords', {
+            statistic: 'Average',
+            unit: cloudwatch.Unit.COUNT
+        });
+        const failedConversionRecords = this.dashboard.createKinesisHoseMetric(hoseName, 'FailedConversion.Records', {
+            statistic: 'Sum',
+            unit: cloudwatch.Unit.BYTES
+        });
+        
+        this.dashboard.addWidgets(
+            this.dashboard.createWidget(`${baseName}-success`, [dataReadFromKinesisStreamRecords, deliveryToS3Records, succeedConversionRecords], 12),
+            this.dashboard.createLeftRightWidget(`${baseName}-fail`, [throttledGetRecords], [failedConversionRecords], 12),
+        );
+    }
+
+    private nagSuppress() {
+        NagSuppressions.addStackSuppressions(this, [
+            {
+                id: 'AwsSolutions-SNS2',
+                reason: 'Demonstrate a stack level suppression.'
+            },
+            {
+                id: 'AwsSolutions-SNS3',
+                reason: 'Demonstrate a stack level suppression.'
+            }
+        ]);
+    }
+}
