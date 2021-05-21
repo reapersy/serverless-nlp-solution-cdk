7f623155e936401190a0775bb6ad3238ebe4ca9e
reapersy <reapersoy@proton.me>
2021-05-21 18:00:16 +0000

    More notes

--- lib/template/app-config.ts
+++ lib/template/app-config.ts
@@ -16,4 +16,22 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-export interfa
+export interface ProjectConfig {
+    Name: string,
+    Stage: string,
+    Account: string,
+    Region: string,
+    Profile: string
+}
+
+export interface StackConfig {
+    Name: string,
+    UpdateRegionName?: string,
+    [name: string]: any
+}
+
+export interface AppConfig {
+    Project: ProjectConfig;
+    Global: any;
+    Stack: StackConfig;
+}
