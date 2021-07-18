52316785a9aca12140b2dff4eed89ffe99b5f6b9
reapersy <reapersoy@proton.me>
2021-07-18 09:48:48 +0000

    cpufreq: interactive: set at least hispeed when above hispeed load

--- lib/template/stack/devops/pipeline-base-stack.ts
+++ lib/template/stack/devops/pipeline-base-stack.ts
@@ -4,4 +4,10 @@
  * SPDX-License-Identifier: MIT-0
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * so
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF ME
