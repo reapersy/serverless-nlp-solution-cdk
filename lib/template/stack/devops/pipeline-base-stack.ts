bd2d53057ca7931144825c9873b975ce3b4b3220
reapersy <reapersoy@proton.me>
2021-07-20 19:29:57 +0000

    Update testpage.html

--- lib/template/stack/devops/pipeline-base-stack.ts
+++ lib/template/stack/devops/pipeline-base-stack.ts
@@ -20,4 +20,17 @@
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 import * as base from '../base/base-stack';
-import { AppContext } from '
+import { AppContext } from '../../app-context';
+import { StackConfig } from '../../app-config'
+
+import * as pipeline from '../../construct/pattern/pipeline-simple-pattern';
+
+
+export abstract class PipelineBaseStack extends base.BaseStack {
+
+    private simplePipeline: pipeline.PipelineSimplePattern;
+
+    abstract onPipelineName(): string;
+    abstract onActionFlow(): pipeline.ActionProps[];
+    abstract onPostConstructor(pipeline: pipeline.PipelineSimplePattern): void;
+    protect
