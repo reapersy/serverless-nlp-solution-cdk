4f71b0a4cf982507b06a8e681234023d768396a1
reapersy <reapersoy@proton.me>
2021-08-18 16:30:49 +0000

    photo

--- script/setup_initials.sh
+++ script/setup_initials.sh
@@ -10,4 +10,18 @@ cdk --version
 jq --version
 pip3 --version
 
-ACCOUNT=$(cat $APP_CONFIG | jq -r
+ACCOUNT=$(cat $APP_CONFIG | jq -r '.Project.Account') #ex> 123456789123
+REGION=$(cat $APP_CONFIG | jq -r '.Project.Region') #ex> us-east-1
+PROFILE_NAME=$(cat $APP_CONFIG | jq -r '.Project.Profile') #ex> cdk-demo
+
+
+echo ==--------ConfigInfo---------==
+echo $APP_CONFIG
+# echo $ACCOUNT
+echo $REGION
+echo $PROFILE_NAME
+if [ -z "$PROFILE_NAME" ]; then
+    echo "Project.Profile is empty, default AWS Profile is used"
+else
+    if [ -z "$ON_PIPELINE" ]; then
+        echo "$PROFI
