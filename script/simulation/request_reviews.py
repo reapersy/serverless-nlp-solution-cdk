716005ccfe53e3e8a004a8ce6db0239aef5dd67a
reapersy <reapersoy@proton.me>
2022-02-12 12:58:11 +0000

    Work on 3.1 packaging

--- script/simulation/request_reviews.py
+++ script/simulation/request_reviews.py
@@ -8,4 +8,16 @@ import boto3
 
 
 '''
-How to execute: python3 script/simul
+How to execute: python3 script/simulation/request_reviews.py --profile [aws profile name] --url [APIGatewaty URL + /review] --pool [cognito user pool client id] --id [new user id] --pw [new user pw]
+'''
+
+
+def login(user_pool, username, password) -> str:
+    client = boto3.client('cognito-idp')
+    response = client.initiate_auth(
+        AuthFlow='USER_PASSWORD_AUTH',
+        AuthParameters={
+            'USERNAME': username,
+            'PASSWORD': password
+        },
+    
