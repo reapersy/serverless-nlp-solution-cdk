b222fd7a1a1940d0ea773c9156cd771e9608040c
reapersy <reapersoy@proton.me>
2022-02-20 15:46:59 +0000

    Update pytest from 3.2.0 to 3.2.1

--- test/cdk-nag.test.ts
+++ test/cdk-nag.test.ts
@@ -77,3 +77,62 @@ describe('cdk-nag AwsSolutionsPack ReviewAnalysisStack', () => {
     let stack: Stack;
 
     beforeAll(() => {
+        // GIVEN
+        const appContext = new AppContext({
+            appConfigFileKey: 'APP_CONFIG',
+        });
+        stack = new ReviewAnalysisStack(appContext, appContext.appConfig.Stack.ReviewAnalysis);
+
+        // WHEN
+        Aspects.of(stack).add(new AwsSolutionsChecks());
+    });
+
+    // THEN
+    test('No unsuppressed Warnings', () => {
+        const warnings = Annotations.fromStack(stack).findWarning(
+            '*',
+            Match.stringLikeRegexp('AwsSolutions-.*')
+        );
+        expect(warnings).toHaveLength(0);
+    });
+
+    test('No unsuppressed Errors', () => {
+        const errors = Annotations.fromStack(stack).findError(
+            '*',
+            Match.stringLikeRegexp('AwsSolutions-.*')
+        );
+        expect(errors).toHaveLength(0);
+    });
+});
+
+describe('cdk-nag AwsSolutionsPack ReviewDashboardStack', () => {
+    let stack: Stack;
+
+    beforeAll(() => {
+        // GIVEN
+        const appContext = new AppContext({
+            appConfigFileKey: 'APP_CONFIG',
+        });
+        stack = new ReviewDashboardStack(appContext, appContext.appConfig.Stack.ReviewDashboard);
+
+        // WHEN
+        Aspects.of(stack).add(new AwsSolutionsChecks());
+    });
+
+    // THEN
+    test('No unsuppressed Warnings', () => {
+        const warnings = Annotations.fromStack(stack).findWarning(
+            '*',
+            Match.stringLikeRegexp('AwsSolutions-.*')
+        );
+        expect(warnings).toHaveLength(0);
+    });
+
+    test('No unsuppressed Errors', () => {
+        const errors = Annotations.fromStack(stack).findError(
+            '*',
+            Match.stringLikeRegexp('AwsSolutions-.*')
+        );
+        expect(errors).toHaveLength(0);
+    });
+});
